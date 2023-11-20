e28453c2212f6fa91a27d705b83ea6c80d55e972
Tinson <107073910+mtinsonk@users.noreply.github.com>
2023-11-20 12:09:25 -0600

    Monikai Testing fork created because of unreachable main repo.

---
+++ Weather_ Application/src/test/java/com/example/weather_application/HelloServletTest.java
@@ -0,0 +1,38 @@
+package com.example.weather_application;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HelloServletTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void doGet() {
+    }
+
+    @Test
+    void getLastModified() {
+    }
+
+    @Test
+    void init() {
+    }
+
+    @Test
+    void testDoGet() {
+    }
+
+    @Test
+    void destroy() {
+    }
+}
